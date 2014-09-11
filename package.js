56ebb9c9141b68e8138c28f039cb143faad446c3
Demyan Rogozhin <demyan.rogozhin@gmail.com>
2014-09-11 21:21:18 +0400

    Upgrade to Meteor v0.9.0 package format

--- package.js
+++ package.js
@@ -1,13 +1,18 @@
 Package.describe({
-    summary: "Straightforward implementation of mongodb aggregation framework"
+    summary: "Straightforward implementation of mongodb aggregation framework",
+    version: "1.0.3",
+    name: "zvictor:mongodb-server-aggregation",
+    git: "https://github.com/zvictor/meteor-mongo-server.git"
 });
 
 Npm.depends({mongodb: "1.3.17"});
 
-Package.on_use(function (api, where) {
-    api.use('coffeescript', ['server']);
-    api.use('underscore', ['server']);
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@0.9.0');
 
-    api.add_files('server.coffee', 'server');
+    api.use('coffeescript', 'server');
+    api.use('underscore', 'server');
+
+    api.addFiles('server.coffee', 'server');
 
 });
